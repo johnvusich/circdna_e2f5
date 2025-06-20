8dea4cc88bcb64aca776a787411cbbb66e7860ef
John Vusich <82066832+johnvusich@users.noreply.github.com>
2025-06-19 21:47:37 -0400

    Update run_circdna.sh

--- run_circdna.sh
+++ run_circdna.sh
@@ -10,16 +10,16 @@ module purge
 module load Nextflow/24.10.2
 
 # Set the relative paths to the genome files
-GENOME_DIR="/mnt/scratch/vusichjo/circdna_e2f5/data_repo/mm10"
+GENOME_DIR="$SCRATCH/circdna_e2f5/data_repo/mm10"
 FASTA="$GENOME_DIR/mm10.fa"
-DATA_REPO="/mnt/scratch/vusichjo/circdna_e2f5/data_repo"
+DATA_REPO="$SCRATCH/circdna_e2f5/data_repo"
 
 # Define the samplesheet, outdir, workdir, and config
-SAMPLESHEET="/mnt/scratch/vusichjo/circdna_e2f5/fetchngs_results/samplesheet/samplesheet.csv"
-OUTDIR="/mnt/scratch/vusichjo/circdna_e2f5/results"
-WORKDIR="/mnt/scratch/vusichjo/circdna_e2f5_work"
-CONFIG="/mnt/scratch/vusichjo/circdna_e2f5/icer.config"
-LICENSE="/mnt/scratch/vusichjo/circdna_e2f5/mosek_license_dir"
+SAMPLESHEET="$SCRATCH/circdna_e2f5/fetchngs_results/samplesheet/samplesheet.csv"
+OUTDIR="$SCRATCH/vusichjo/circdna_e2f5/results"
+WORKDIR="$SCRATCH/vusichjo/circdna_e2f5_work"
+CONFIG="$SCRATCH/vusichjo/circdna_e2f5/icer.config"
+LICENSE="$SCRATCH/vusichjo/circdna_e2f5/mosek_license_dir"
 
 # Run the differential abundance analysis
 nextflow run nf-core/circdna -r 1.1.0 -profile singularity -work-dir $WORKDIR -resume \
