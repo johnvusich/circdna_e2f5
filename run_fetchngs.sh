2845edb2027a1666b7fcc92eae67f8122a32f2b5
John Vusich <82066832+johnvusich@users.noreply.github.com>
2025-07-03 15:08:37 -0400

    Update run_fetchngs.sh
    Update with flexible file paths "base_dir" and use most updated Nextflow version by not specifying the version

--- run_fetchngs.sh
+++ run_fetchngs.sh
@@ -1,24 +1,37 @@
 #!/bin/bash --login
-#SBATCH --job-name=fetchngs_e2f5
-#SBATCH --time=3:59:00
+#SBATCH --job-name=circdna_e2f5
+#SBATCH --time=24:00:00
 #SBATCH --mem=4GB
 #SBATCH --cpus-per-task=1
-#SBATCH --output=fetchngs_e2f5-%j.out
+#SBATCH --output=circdna_e2f5-%j.out
 
 # Load Nextflow
 module purge
 module load Nextflow
 
-# Define the samplesheet, outdir, workdir, and config
-SAMPLESHEET="$SCRATCH/circdna_e2f5/ids.csv" 
-OUTDIR="$SCRATCH/circdna_e2f5/fetchngs_results" 
-WORKDIR="$SCRATCH/circdna_e2f5_work"
-CONFIG="$SCRATCH/circdna_e2f5/icer.config"
+# Set base directory (default to current dir if not provided)
+BASE_DIR=${1:-$PWD}
 
-# Run the pipeline
-nextflow pull nf-core/fetchngs
-nextflow run nf-core/fetchngs -r 1.12.0 -profile singularity -work-dir $WORKDIR -resume \
---input $SAMPLESHEET \
---outdir $OUTDIR \
---download_method 'sratools' \
--c $CONFIG
+# Set the relative paths to the genome files
+GENOME_DIR="$BASE_DIR/data_repo/mm10"
+FASTA="$GENOME_DIR/mm10.fa"
+DATA_REPO="$BASE_DIR/data_repo"
+
+# Define the samplesheet, outdir, workdir, and config
+SAMPLESHEET="$BASE_DIR/fetchngs_results/samplesheet/samplesheet.csv"
+OUTDIR="$BASE_DIR/results"
+WORKDIR="$BASE_DIR/work"
+CONFIG="$BASE_DIR/icer.config"
+LICENSE="$BASE_DIR/mosek_license_dir"
+
+# Run the circDNA pipeline
+nextflow run nf-core/circdna -r 1.1.0 -profile singularity -work-dir $WORKDIR -resume \
+  --input $SAMPLESHEET \
+  --outdir $OUTDIR \
+  --fasta $FASTA \
+  --input_format 'FASTQ' \
+  --circle_identifier 'ampliconarchitect,circle_finder,circexplorer2' \
+  --mosek_license_dir $LICENSE \
+  --aa_data_repo $DATA_REPO \
+  --reference_build 'mm10' \
+  -c $CONFIG
