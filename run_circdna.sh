b7ceb80f50d0481fc0621d8a3af00b8a3f33bd15
John Vusich <82066832+johnvusich@users.noreply.github.com>
2025-07-03 15:17:36 -0400

    Update run_circdna.sh

--- run_circdna.sh
+++ run_circdna.sh
@@ -7,28 +7,31 @@
 
 # Load Nextflow
 module purge
-module load Nextflow/24.10.2
+module load Nextflow
+
+# Set base directory (default to current dir if not provided)
+BASE_DIR=${1:-$PWD}
 
 # Set the relative paths to the genome files
-GENOME_DIR="$SCRATCH/circdna_e2f5/data_repo/mm10"
+GENOME_DIR="$BASE_DIR/data_repo/mm10"
 FASTA="$GENOME_DIR/mm10.fa"
-DATA_REPO="$SCRATCH/circdna_e2f5/data_repo"
+DATA_REPO="$BASE_DIR/data_repo"
 
 # Define the samplesheet, outdir, workdir, and config
-SAMPLESHEET="$SCRATCH/circdna_e2f5/fetchngs_results/samplesheet/samplesheet.csv"
-OUTDIR="$SCRATCH/vusichjo/circdna_e2f5/results"
-WORKDIR="$SCRATCH/vusichjo/circdna_e2f5_work"
-CONFIG="$SCRATCH/vusichjo/circdna_e2f5/icer.config"
-LICENSE="$SCRATCH/vusichjo/circdna_e2f5/mosek_license_dir"
+SAMPLESHEET="$BASE_DIR/fetchngs_results/samplesheet/samplesheet.csv"
+OUTDIR="$BASE_DIR/results"
+WORKDIR="$BASE_DIR/work"
+CONFIG="$BASE_DIR/icer.config"
+LICENSE="$BASE_DIR/mosek_license_dir"
 
-# Run the differential abundance analysis
+# Run the circDNA pipeline
 nextflow run nf-core/circdna -r 1.1.0 -profile singularity -work-dir $WORKDIR -resume \
---input $SAMPLESHEET \
---outdir $OUTDIR \
---fasta $FASTA \
---input_format 'FASTQ' \
---circle_identifier 'ampliconarchitect,circle_finder,circexplorer2' \
---mosek_license_dir $LICENSE \
---aa_data_repo $DATA_REPO \
---reference_build 'mm10' \
--c $CONFIG
+  --input $SAMPLESHEET \
+  --outdir $OUTDIR \
+  --fasta $FASTA \
+  --input_format 'FASTQ' \
+  --circle_identifier 'ampliconarchitect,circle_finder,circexplorer2' \
+  --mosek_license_dir $LICENSE \
+  --aa_data_repo $DATA_REPO \
+  --reference_build 'mm10' \
+  -c $CONFIG
